b442178b7e3dd52bd5e8f4c44b6b53c4ece444a3
Ian Maulana <ian_maulana@yahoo.com>
2021-07-04 21:45:39 +0700

    Add tag props on Box components

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BoxStyled from '@components/Box/Box.styled';
 
-const Box = React.forwardRef(({ children, ...props }, ref) => (
+const Box = React.forwardRef(({ children, tag, ...props }, ref) => (
   <>
-    <BoxStyled {...props} ref={ref}>
+    <BoxStyled as={tag} {...props} ref={ref}>
       {children}
     </BoxStyled>
   </>
 ));
 
 Box.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
+  tag: PropTypes.string,
+};
+
+Box.defaultProps = {
+  children: '',
+  tag: 'div',
 };
 
 export default Box;
